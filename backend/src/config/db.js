3becb86197679f8c6aa67453e4d2246579f3ffdc
idir <idirb2005@gmail.com>
2026-02-21 12:11:45 +0000

    var changes db.js added cosnt MONGO_URI instead of process.env.MONGOURI

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose"
 
+const MONGO_URI = process.env.MONGO_URI
+
 export const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(MONGO_URI)
         console.log("MONGODB CONNECTED SUCCESSFULLY!!");
     }catch(error){
         console.error('Error connecting mongoDB', error);
