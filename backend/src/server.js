458776424e761201bd34579322c68a59962c588e
idir <idirb2005@gmail.com>
2026-02-21 11:58:47 +0000

    setup .env (PORT, MONGO_URI), setup db.js for connecting to db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express" //to is this syntax had to change type : "module" else use => const express = require("express");
 import notesRouter from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use("/api/note", notesRouter);
 
-app.listen(5001, () =>{
-    console.log("port 5001");
+connectDB;
+
+app.listen(PORT, () =>{
+    console.log("port ", PORT);
 })
